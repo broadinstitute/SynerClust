68f249f092fd9df6829c28b193d88960a13c1dd1
c_georgescu <christophe93g@gmail.com>
2016-02-04 14:00:02 -0500

    Fix

--- src/COBRA_Repo_Handling.py
+++ src/COBRA_Repo_Handling.py
@@ -47,18 +47,18 @@ class RepoParse:
 						seq = None
 						if os.path.isfile(repo_path + dat[1]): # path relative to repo, "./" and "../" can be used
 							RepoParse.logger.warning("Are you sure \"%s\" is a gff3 annotation file?" %(dat[1]))
+							seq = repo_path + dat[1] + ".genome"
 							dat[1] = repo_path + dat[1]
-							seq = repo_path + dat[1] + ".genome"
 						elif os.path.isfile(repo_path + dat[1] + ".annotation.gff3"):
-							dat[1] = repo_path + dat[1] + ".annotation.gff3"
 							seq = repo_path + dat[1] + ".genome"
+							dat[1] = repo_path + dat[1] + ".annotation.gff3"
 						elif os.path.isfile(repo_path + curGenome["Genome"] + "/" + dat[1]):
 							RepoParse.logger.warning("Are you sure \"%s\" is a gff3 annotation file?" %(dat[1]))
+							seq = repo_path + curGenome["Genome"] + "/" + dat[1] + ".genome"
 							dat[1] = repo_path + curGenome["Genome"] + "/" + dat[1]
-							seq = repo_path + curGenome["Genome"] + "/" + dat[1] + ".genome"
 						elif os.path.isfile(repo_path + curGenome["Genome"] + "/" + dat[1] + ".annotation.gff3"):
-							dat[1] = repo_path + curGenome["Genome"] + "/" + dat[1] + ".annotation.gff3"
 							seq = repo_path + curGenome["Genome"] + "/" + dat[1] + ".genome"
+							dat[1] = repo_path + curGenome["Genome"] + "/" + dat[1] + ".annotation.gff3"
 						else:
 							RepoParse.logger.error("Specified annotation file not found for %s" %(curGenome["Genome"]))
 							sys.exit("Specified annotation file not found for %s" %(curGenome["Genome"]))
@@ -136,7 +136,7 @@ class RepoParse:
 	def makeGenomeDirectories(self, genome_dir, distribute,synteny_window):
 		command_lists = []
 		for g in self.genomes:
-			ret = g.setupDirectory(genome_dir,distribute,synteny_window)
+			ret = g.setupDirectory(genome_dir, distribute, synteny_window)
 			if ret:
 				command_lists.append(ret)
 		if len(command_lists) >0:
@@ -162,7 +162,7 @@ class Genome:
 		self.directory = ""
 		Genome.logger.debug("Genome Initialized")
 		
-	def setupDirectory(self, genomeDir,distribute,synteny_window):
+	def setupDirectory(self, genomeDir, distribute, synteny_window):
 		#make a directory for this genome in the genomeDir
 		myCommands = []
 		myDir = genomeDir+self.genome+"/"
