fb1c31cd866d9549d1f1259dbb9bc099ee605ed1
c_georgescu <christophe93g@gmail.com>
2016-05-02 16:40:16 -0400

    Type cast.

--- src/WF_Initialization.py
+++ src/WF_Initialization.py
@@ -327,10 +327,10 @@ class Tree:
 		with open(working_dir + "uger_jobs.txt", "w") as out:
 			while len(stack) > 0:
 				current = stack.pop()
-				out.write("qsub -N #TIMESTAMP" + current[1])
+				out.write("qsub -N #TIMESTAMP" + str(current[1]))
 				if len(current[2]) != 0:
-					out.write("-hold_jid #TIMESTAMP" + current[2][0] + ",#TIMESTAMP" + current[2][1])
-				out.write(" " + working_dir + "/nodes/" + current[0] + "/" + current[0] + ".sh\n")
+					out.write("-hold_jid #TIMESTAMP" + str(current[2][0]) + ",#TIMESTAMP" + str(current[2][1]))
+				out.write(" " + working_dir + "/nodes/" + str(current[0]) + "/" + str(current[0]) + ".sh\n")
 
 		all_proc_nodes = []
 		serial_sets = {}
